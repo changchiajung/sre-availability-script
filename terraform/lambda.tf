cbf7cb01e80cc81957b494cc14ce25a8ccadd36b
changchiajung <rabbit20058@gmail.com>
2024-01-23 23:34:29 -0700

    Update lambda image tag

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,7 +1,7 @@
 resource "aws_lambda_function" "healthcheck_lambda" {
   function_name = var.lambda_name
   timeout       = var.lambda_timeout
-  image_uri     = "${aws_ecr_repository.lambda_repo.repository_url}:${var.lambda_image_tag}"
+  image_uri     = "${aws_ecr_repository.lambda_repo.repository_url}:${coalesce(local.lambda_image_tag, var.lambda_image_tag)}"
   package_type  = "Image"
 
   role = aws_iam_role.iam_for_lambda.arn
---
+++ terraform/locals.tf
@@ -0,0 +1,3 @@
+locals {
+  lambda_image_tag = "build-31de5c50-75a1-42b7-8f2e-72ba06903b79"
+}
